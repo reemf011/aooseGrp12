00ee53ab9ceaaf879f38468418b2c55fbdb85bd7
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-11-29 12:58:58 +0300

    Reem's commit

--- src/githubdemo/GithubDemo.java
+++ src/githubdemo/GithubDemo.java
@@ -16,7 +16,7 @@ public class GithubDemo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(" aoose, farah");
+        System.out.println(" aoose, Reem");
        
     }
     
