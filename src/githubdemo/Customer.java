7cc2eb1e3bbfc3f3aded524c12f3a17f552ac1a1
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-12-03 14:26:45 +0300

    reem edit

--- src/githubdemo/Customer.java
+++ src/githubdemo/Customer.java
@@ -11,7 +11,7 @@ package githubdemo;
  */
 public class Customer implements  Observer  {
     String creditCardinfo;
-  System.out.println("ALERT!!: EXCLUSIVE DISCOUNT");
+  
        @Override
        public void update(float SpecialOffers) {
               System.out.println("ALERT!!: EXCLUSIVE DISCOUNT"+ SpecialOffers+ "%");
